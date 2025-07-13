c5eb2c7c8c486540926eedef39234698d5c246ea
Channu-22 <channusinnur22072002@gmail.com>
2025-07-13 16:04:33 +0530

    implemented routers successsfully

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 import Products from './pages/Home.jsx'
 import Router from './Router.jsx'
 
